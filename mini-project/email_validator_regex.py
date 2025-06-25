a26c994308fae135dfb2097eda652e3afdd8ff47
RamParmar1 <ramp19127@outlook.com>
2025-06-25 23:27:11 +0530

    Added email-validator-regex File

---
+++ mini-project/email_validator_regex.py
@@ -0,0 +1,24 @@
+# Email Validation Usingn regex module
+
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email =input('Enter  your Email : ')
+
+if re.search(email_condition,user_email):
+    print("Right Email")
+else:
+    print("Wrong Email")
+
+#Working
+'''Here in variable,email_condition check that it meets condition or not,
+then check through te regex own funcationality (search),
+it will search our input value/string in the variable(email_condition) which define and find condition,
+if true then get result of (Right Email) 
+or false then get (Wrong Email)
+'''
+#Condition's
+'''a-z ramparmar@gmail.com
+   0-9
+   . And _ at a one time
+   @ at a one time
+   . in position from last 2 And 3'''
