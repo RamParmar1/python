485a98a7bfa0531531c01e33d7a4be6123a1a6b3
RamParmar1 <ramp19127@outlook.com>
2025-06-30 23:48:27 +0530

    Added qrcode_generator File

--- mini-project/qrcode_generator.py
+++ mini-project/qrcode_generator.py
@@ -1,19 +1,12 @@
 #qr code generator
 
-#create qr
 import qrcode as qr
-img = qr.make("https://github.com/RamParmar1/python/mini-project/qrcode_generator")
-img.save('Mine_GitHub.png') #give name that you want
-
-#create and enhance qr
-#qr as variable/function
-'''import qrcode
+import os
 from PIL import Image
-qr = qrcode.QRCode( version=1 ,
-                    error_correction = qrcode.constants.ERROR_CORRECT_H ,
-                    box_size = 10 , border = 10  )
-qr.add_data("https://github.com/RamParmar1/python/mini-project/qrcode_generator")
-qr.make(fit = True)
-img = qr.make_Image(fill_color = "red" , back_color = "white")
-img.save("Mine_GitHub.png")
-'''
+
+img = qr.make("https://github.com/RamParmar1/python/mini-project/qrcode_generator")
+img.save('Mine_GitHub.png')
+print("Image Done")
+img = Image.open("Mine_GitHub.png")
+img.show()
+
