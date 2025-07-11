1175bf210572eb787c5d6caccd83dc6907705f13
RamParmar1 <ramp19127@outlook.com>
2025-07-11 23:19:45 +0530

    Added payment-qrcode File

---
+++ mini-project/payment_qrcode.py
@@ -0,0 +1,25 @@
+#Take Payment Online Qrcode
+
+import qrcode
+
+#Taking UPI ID As a input
+upi_id = input("Enter you UPI ID = ")
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#Create QR Codes for each payment app
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+#Save the QR Code to image file (optional)
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+#Display the QR Codes (you may need to install PIL/Pillow Library)
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
